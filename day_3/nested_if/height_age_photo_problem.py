fd6f95584f41987a31df7bd1bb7e1f4946a06c55
Arpit Jain <arpitjain@Arpits-MacBook-Air.local>
2026-03-30 11:11:26 +0530

    adding height problem changes

--- day_3/nested_if/height_age_photo_problem.py
+++ day_3/nested_if/height_age_photo_problem.py
@@ -4,7 +4,11 @@ if height < 120:
 else:
     bill = 0
     age = int(input("Enter your age : "))
-    if age < 12:
+    if 45 <= age <= 55:
+        print("Everything is going to be OK, Get a FREE-RIDE from us !!!!!")
+        bill = 0
+        wants_photo = "N/A"
+    elif age < 12:
         print("pay $5")
         bill = 5
     elif 12 > age < 18:
@@ -13,9 +17,10 @@ else:
     else:
         print("pay $12")
         bill = 12
-    wants_photo = input(
-        "Do you want to take a photo ? ($3 will be charged extra)\nType Y for Yes or N for No : "
-    )
-    if wants_photo.upper() == "Y":
-        bill += 3
+    if not (45 <= age <= 55):
+        wants_photo = input("Do you want to take a photo? ($3 extra) Y or N: ")
+        if wants_photo.upper() == "Y":
+            bill += 3
+    else:
+        print("Your free photo has been added to your session.")
 print(f"Your total bill is : {bill}")
