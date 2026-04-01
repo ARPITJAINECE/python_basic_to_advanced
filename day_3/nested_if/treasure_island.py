b48e38ce44678573f5075bd17e8b2af0f213178d
Arpit Jain <20501@bandhanlife.com>
2026-04-01 07:56:24 +0530

    adding basic structure of treasure island

--- day_3/nested_if/treasure_island.py
+++ day_3/nested_if/treasure_island.py
@@ -1,1 +1,17 @@
-print("hi")
+print(
+    "welcome to treasure island......\nYour mission is to find the treasure!!!!!!!!!!!!"
+)
+direction = input("Enter the direction : Left or Right ???????????")
+if direction.lower() == "right":
+    print("Game over")
+    exit()
+else:
+    swim_wait = input("whether you want to swim or wait ?")
+    if swim_wait.lower() == "swim":
+        print("game over")
+    else:
+        door = input("which door you want to enter ??? either red, yellow or blue???")
+        if door.lower() == "blue" or door.lower() == "red":
+            print("game over!!!!!!!!!")
+        else:
+            print("you won, hurray!!!!!!!!!!!!!!!!!!!!!!!!!")
