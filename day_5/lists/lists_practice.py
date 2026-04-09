25b531f9cf5f6cbe4adfd69195ae0d54a0db9c55
Arpit Jain <20501@bandhanlife.com>
2026-04-09 11:06:35 +0530

    adding lists basics

--- day_5/lists/lists_practice.py
+++ day_5/lists/lists_practice.py
@@ -1,3 +1,4 @@
 states_of_india = ["rajasthan", "UP", "MP"]
 print(f"states of india are : {states_of_india}")
 print(f"first state of india is : {states_of_india[0]}")
+print(f"last state in india is : {states_of_india[-1]}")
---
+++ day_5/lists/who_pays_bill.py
@@ -0,0 +1,4 @@
+import random
+
+friends = ["arpit", "ruchi", "dipen", "saurabh", "tanmay"]
+print(f"bill will be payed by : {random.choice(friends)}")
