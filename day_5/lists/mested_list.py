4ae11f2cad3ce94fbe0116556cb684dda76f198d
Arpit Jain <20501@bandhanlife.com>
2026-04-30 10:56:57 +0530

    adding misc list and dict

---
+++ day_5/lists/mested_list.py
@@ -0,0 +1,8 @@
+nested_list = [
+    "1",
+    "2",
+    ["3", 4],
+]
+print(nested_list)
+print(nested_list[2])
+print(nested_list[2][1])
---
+++ day_9_dictionaries/misc.py
@@ -0,0 +1,10 @@
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin"],
+    "India": {
+        "Rajasthan": {"Jaipur", "Jodhpur"},
+        "Maharashtra": {"Pune", "Mumbai"},
+    },
+}
+print(travel_log["France"][1])
+print(travel_log["India"]["Rajasthan"])
