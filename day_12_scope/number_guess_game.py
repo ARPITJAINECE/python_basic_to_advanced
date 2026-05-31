8c30577320133b9eb2102b8db3d5636d216133de
Arpit Jain <arpitjain@Arpits-MacBook-Air.local>
2026-05-31 18:55:36 +0530

    adding basic template for number guessing game.

---
+++ day_12_scope/number_guess_game.py
@@ -0,0 +1,26 @@
+def entry():
+    return "Welcome to the number guessing game!!!"
+
+
+def choose_level():
+    print("Do you want hard level or easy level???")
+    print("Press 'h' for hard and 'e' for easy")
+    level = input("Enter your level type : ")
+    return level
+
+
+def hard_level():
+    print("You have choosen hard level...\n Lets start game::::::")
+    chances = 5
+    print(f"You have {chances} left")
+
+
+if __name__ == "__main__":
+    entry()
+    choosen_level = choose_level()
+    if choosen_level.lower() == "h":
+        hard_level()
+    elif choosen_level.lower() == "e":
+        easy_level()
+    else:
+        print("Please enter correct value!!!")
