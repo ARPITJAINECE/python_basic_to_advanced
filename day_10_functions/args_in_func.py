fb4d4873a83c59ab38ed9ef05aea70e61ee58930
ARPITJAINECE <aj241997@gmail.com>
2026-06-02 19:19:58 +0530

    adding function

--- day_10_functions/args_in_func.py
+++ day_10_functions/args_in_func.py
@@ -1,4 +1,9 @@
-sum_of_number = lambda *args: sum(args)
+# sum_of_number = lambda *args: sum(args)
+def sum_of_number(*args):
+    # print(args)
+    return sum(args)
+
+
 print(f"sum is : {sum_of_number(1,2)}")
 print(f"sum is : {sum_of_number(1,2,3)}")
 print(f"sum is : {sum_of_number(1,2,3,4)}")
