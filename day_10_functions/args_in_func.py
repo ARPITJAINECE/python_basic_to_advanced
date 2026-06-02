8ea96931fea17428a3e1930f434377a578dc2aea
ARPITJAINECE <aj241997@gmail.com>
2026-06-02 11:51:33 +0530

    adding multiple args in functions

---
+++ day_10_functions/args_in_func.py
@@ -0,0 +1,6 @@
+sum_of_number = lambda *args: sum(args)
+print(f"sum is : {sum_of_number(1,2)}")
+print(f"sum is : {sum_of_number(1,2,3)}")
+print(f"sum is : {sum_of_number(1,2,3,4)}")
+print(f"sum is : {sum_of_number(1,2,3,4,5)}")
+print(f"sum is : {sum_of_number(1,2,3,4,5,6)}")
